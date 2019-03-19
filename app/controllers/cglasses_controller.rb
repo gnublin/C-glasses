56e7f0ca19cdd1446e20f50cecf2dd6d3cb2868d
Gauthier FRANÇOIS <gauthier.francois@doctolib.com>
2019-03-19 08:24:17 +0100

    feat(#1): rails env setup mini

---
+++ app/controllers/cglasses_controller.rb
@@ -0,0 +1,6 @@
+class CglassesController < ApplicationController
+
+  def home
+    @ta = "Hey! How are you?"
+  end
+end
---
+++ test/controllers/cglasses_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CglassesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
