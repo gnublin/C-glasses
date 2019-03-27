962d45b858704b0fe2f5e0fa8669b7d8a4023f67
Gauthier FRANÇOIS <gauthier.francois@doctolib.com>
2019-03-27 14:00:23 +0100

    add crontroller

--- app/controllers/cglasses_controller.rb
+++ app/controllers/cglasses_controller.rb
@@ -3,6 +3,9 @@
 
 class CglassesController < ApplicationController
   def home
-    @run = %x(ls #{params[:run]})
+    url = params[:url]
+    @phantomas_data = %x(curl  -o /dev/null -s -w 'Total: %{time_total}s\n'  url)
+    website_new = Website.create!(url: url)
+    website_new.phantomas_runs.create!(data: @phantomas_data)
   end
 end
